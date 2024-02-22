92f10754d54bc18b10f46e370fab692d2675cf69
rohit1996 <rrsonawne090909@gmail.com>
2024-02-22 09:46:08 -0800

    order change

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import "express-async-errors";
-import dotenv from "dotenv";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@rscom/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
-import { updateTicketRouter } from "./routes/update";
-dotenv.config();
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes/index";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -20,10 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
